c1d75ffbbb5dcc3e4bc62f375d1a0264ff0f0785
M4theusRibeiro <matheusribeiro3105@gmail.com>
2025-02-09 00:58:07 -0300

    Inicio da section dos personagens

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CardPersonagemComponent } from "../card-personagem/card-personagem.component";
+import { Personagem } from '../../models/Personagem';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavbarComponent, CardPersonagemComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  standalone: true
 })
 export class HomeComponent {
+
+
+  madeline: Personagem = {nome: 'Madeline', imagem: '../../../assets/img/personagensGif/madeline/normal.gif', cor: 'red', descricao: 'Madeline é a protagonista do jogo Celeste. Ela é uma jovem mulher que decide escalar a montanha Celeste. Madeline é uma personagem muito carismática e determinada.'};
 
 }
